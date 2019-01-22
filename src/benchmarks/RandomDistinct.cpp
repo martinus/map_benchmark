1b5fda4a803d5be6a71e9e2582fdb0cd2fcebdf3
Martin Ankerl <martin.ankerl@gmail.com>
2019-01-22 18:49:59 +0100

    update benchmark

--- src/benchmarks/RandomDistinct.cpp
+++ src/benchmarks/RandomDistinct.cpp
@@ -2,33 +2,54 @@
 
 #include "bench.h"
 
-static void RandomDistinct(Bench& bench) {
-	bench.title("RandomDistinct");
-	bench.description("25% distinct entries. Adapted from https://github.com/attractivechaos/udb2");
+static void RandomDistinct2(Bench& bench) {
+	bench.title("RandomDistinct2");
+	static size_t const n = 50'000'000;
 
 	auto& rng = bench.rng();
 
-	size_t checksum = 0;
-	size_t const upper = 70'000'000;
-	size_t const lower = 10'000'000;
-	size_t const num_steps = 7;
-	size_t const step_width = (upper - lower) / num_steps;
-
+	int checksum = 0;
 	bench.beginMeasure();
-	for (size_t n = lower; n <= upper; n += step_width) {
-		size_t const max_rng = n / 4;
-		{
-			bench.event("start");
-			Map<int, int> map;
-			for (size_t i = 0; i < n; ++i) {
-				checksum += ++map[static_cast<int>(rng(max_rng))];
-			}
-			bench.event("finish");
+	{
+		size_t const max_rng = n / 20;
+		Map<int, int> map;
+		for (size_t i = 0; i < n; ++i) {
+			checksum += ++map[static_cast<int>(rng(max_rng))];
 		}
+		bench.event("5% distinct");
 	}
-	bench.event("done");
+	bench.event("dtor");
+
+	{
+		size_t const max_rng = n / 4;
+		Map<int, int> map;
+		for (size_t i = 0; i < n; ++i) {
+			checksum += ++map[static_cast<int>(rng(max_rng))];
+		}
+		bench.event("25% distinct");
+	}
+	bench.event("dtor");
+
+	{
+		size_t const max_rng = n / 2;
+		Map<int, int> map;
+		for (size_t i = 0; i < n; ++i) {
+			checksum += ++map[static_cast<int>(rng(max_rng))];
+		}
+		bench.event("50% distinct");
+	}
+	bench.event("dtor");
+
+	{
+		Map<int, int> map;
+		for (size_t i = 0; i < n; ++i) {
+			checksum += ++map[static_cast<int>(rng())];
+		}
+		bench.event("100% distinct");
+	}
+	bench.event("dtor");
 	bench.endMeasure();
-	bench.result(0xeb69f3ce52c0a705, checksum);
+	bench.result(0xb054cacf033c42bd, checksum);
 }
 
-static BenchRegister reg(RandomDistinct);
+static BenchRegister reg(RandomDistinct2);
