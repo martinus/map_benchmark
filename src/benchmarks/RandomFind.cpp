0276d6c2da67ecf882d24c3b077d6bab60548fb3
Martin Ankerl <martin.ankerl@gmail.com>
2019-01-27 14:02:47 +0100

    simplified random find: now single find, with 50% probability of found.

--- src/benchmarks/RandomFind.cpp
+++ src/benchmarks/RandomFind.cpp
@@ -1,5 +1,6 @@
 #include "map_defines.h"
 
+#include "RandomBool.h"
 #include "bench.h"
 
 template <class Key, class Val>
@@ -39,4 +40,29 @@ static void RandomFindNonexisting(Bench& bench) {
 	bench.result(0x881ae8dd442c712b, result);
 }
 
-static BenchRegister reg(RandomFindNonexisting);
+static void RandomFind(Bench& bench) {
+	bench.title("RandomFind");
+	auto& rng = bench.rng();
+	RandomBool rbool;
+
+	size_t num_found = 0;
+	bench.beginMeasure();
+	{
+		Map<size_t, size_t> map;
+		for (size_t i = 1; i <= 1000000; ++i) {
+			if (rbool(rng)) {
+				map.emplace(i, i);
+			}
+
+			for (size_t j = 0; j < 1000; ++j) {
+				num_found += map.count(static_cast<size_t>(rng(i)));
+			}
+		}
+		bench.event("done");
+	}
+	bench.event("dtor");
+	bench.endMeasure();
+	bench.result(0xd8a88adc94b169d6, num_found);
+}
+
+static BenchRegister reg(RandomFind);
