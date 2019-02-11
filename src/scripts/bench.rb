0e9beb3ca6556ff0b3bdb9418683752c29964f4b
Martin Ankerl <martin.ankerl@gmail.com>
2019-02-11 21:33:53 +0100

    better benchmark order

--- src/scripts/bench.rb
+++ src/scripts/bench.rb
@@ -2,13 +2,15 @@
 
 ENV["LD_LIBRARY_PATH"] = "${LD_LIBRARY_PATH}:/home/martinus/dev/abseil-cpp/bazel-bin/absl/container:/home/martinus/dev/abseil-cpp/bazel-bin/absl/hash"
 
-Dir["bench*"].sort.each do |f|
-    list = `./#{f} l`
-    list.split("\n").each do |l|
+list = `./#{Dir["bench*"].first} l`.split("\n")
+
+list.each do |l|
+    #next unless l =~ /RandomFind/
+    Dir["bench*"].sort.each do |f|
         cmd = "timeout 10m ./#{f} f \"^#{l}$\""
-        #puts cmd
         if !system(cmd)
             puts "TIMEOUT: #{f} #{l}"
         end
     end
+    puts
 end
