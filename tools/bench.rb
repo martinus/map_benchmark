a27f85ecf7e3c710fb9b9417b722679d8a47a553
Martin Ankerl <martin.ankerl@dynatrace.com>
2019-02-18 12:12:09 +0100

    fixed script

--- tools/bench.rb
+++ tools/bench.rb
@@ -1,23 +1,17 @@
 #!/usr/bin/env ruby
 
-require "pp"
-
-ENV["LD_LIBRARY_PATH"] = "${LD_LIBRARY_PATH}:/home/martinus/dev/abseil-cpp/bazel-bin/absl/container:/home/martinus/dev/abseil-cpp/bazel-bin/absl/hash"
-
 benchs = `./#{Dir["bench*"].first} l`.split("\n")
-
 apps = Dir["bench*"].sort.uniq
 
 STDERR.puts "running these apps:\n\t#{apps.join("\t\n")}"
 
-10.times do |i|
-    benchs.each do |l|
-        # next unless l =~ /RandomFind/
-        STDERR.puts "iteration #{i}"
-        apps.each do |f|
-            cmd = "timeout 10m ./#{f} f \"^#{l}$\""
+10.times do |iter|
+    benchs.each do |bench|
+        STDERR.puts "iteration #{iter}"
+        apps.each do |app|
+            cmd = "timeout 10m ./#{app} #{bench}"
             if !system(cmd)
-                puts "TIMEOUT: #{f} #{l}"
+                puts "TIMEOUT: #{app} #{bench}"
             end
             sleep 1
         end
