ac041b4aacdb47b91399ca92e0dd057ee22e1aa5
Martin Ankerl <martin.ankerl@dynatrace.com>
2019-02-18 14:35:36 +0100

    renamed hash to identiy

--- src/hashes/IdentityHash/Hash.h
+++ src/hashes/IdentityHash/Hash.h
@@ -4,9 +4,9 @@
 #include <cstdint>
 #include <type_traits>
 
-static const char* HashName = "Null";
+static const char* HashName = "Identity";
 
-// Linux uses a hash that does nothing. It's obviously fast, but extemely bad hash. Some hashmap implementations can't deal at all with this.
+// Linux uses an identity hash that does nothing. It's obviously fast, but extemely bad hash. Some hashmap implementations can't deal at all with this.
 template <typename T>
 struct Hash {
 	size_t operator()(T const& key) const {
