9ce0bbc146c73e329d0e2c230f821f3bc8fabda8
Martin Ankerl <martin.ankerl@gmail.com>
2019-03-14 19:55:54 +0100

    show version for boost map

--- src/maps/boost_unordered_map/Map.h
+++ src/maps/boost_unordered_map/Map.h
@@ -2,8 +2,9 @@
 
 #include "Hash.h"
 #include <boost/unordered_map.hpp>
+#include <boost/version.hpp>
 
-static const char* MapName = "boost::unordered_map";
+static const char* MapName = "boost::unordered_map " BOOST_LIB_VERSION;
 
 template <class Key, class Val>
 using Map = boost::unordered_map<Key, Val, Hash<Key>>;
