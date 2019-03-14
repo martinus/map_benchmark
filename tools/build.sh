1ae8b84fc0febaf440028a979e506ac0bbb4f700
Martin Ankerl <martin.ankerl@gmail.com>
2019-03-14 19:56:03 +0100

    replaced build script

---
+++ tools/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+SOURCEDIR=$(dirname "$0")/..
+BUILDDIR=${SOURCEDIR}/build
+
+cd ${BUILDDIR}
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++-8" -G Ninja ${SOURCEDIR}
+
+ninja
--- tools/cmake_release_here.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-SOURCEDIR=$(dirname "$0")/..
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++-8" -G Ninja ${SOURCEDIR}
