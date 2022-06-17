e5d4ffd679375d3f7391cc03eec82c41fef48018
Martin Leitner-Ankerl <martin.leitner-ankerl@dynatrace.com>
2022-06-17 21:22:49 +0200

    fix cmake

--- src/maps/boost_unordered_map_unsynchronized_pool_resource/dependencies.cmake
+++ src/maps/boost_unordered_map_unsynchronized_pool_resource/dependencies.cmake
@@ -1,3 +1,2 @@
 target_include_directories(${EXECUTABLE_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
-message(${Boost_LIBRARIES})
-target_link_libraries(${EXECUTABLE_NAME} LINK_PUBLIC ${Boost_LIBRARIES} )
+target_link_libraries(${EXECUTABLE_NAME} ${Boost_LIBRARIES})
