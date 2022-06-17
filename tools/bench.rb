80fefc51613132f0c67dc713afed986ab61d9e97
Martin Leitner-Ankerl <martin.leitner-ankerl@dynatrace.com>
2022-06-17 21:23:08 +0200

    skip boost::hash for non-String benchmarks. It's the same as std::hash

--- tools/bench.rb
+++ tools/bench.rb
@@ -11,10 +11,12 @@ if benchs.empty?
     benchs = `./#{Dir["bench*"].first} l`.split("\n")
 end
 apps = Dir["bench*"].sort.uniq
-#apps = apps.shuffle
+
+apps = apps.shuffle
 
 STDERR.puts "apps:\n\t#{apps.join("\n\t")}"
 STDERR.puts "benchmarks:\n\t#{benchs.join("\n\t")}"
+
 
 bad_commands = {}
 
@@ -23,6 +25,12 @@ bad_commands = {}
         STDERR.puts
         STDERR.puts "iteration #{iter}"
         apps.each do |app|
+
+            # filter: benchmarks with 'String' run all hashes, benchmarks *without* don't run boost::hash, because std::hash and boost::hash is the same in that case.
+            if !(bench =~ /String/) && app =~ /boost_hash/
+                puts "SKIPPING #{app} #{bench} because"
+            end
+
             cmd = "#{cmd_prefix} ./#{app} #{bench}"
             if (bad_commands.key?(cmd))
                 puts "SKIPPING #{app} #{bench}"
