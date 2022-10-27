6b87fba150471e197fc5c7a02e858b6c3fff8df4
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-10-27 19:25:57 +0200

    updated externals, boost::unordered_flat_map compiles

--- src/maps/boost_unordered_flat_map/Map.h
+++ src/maps/boost_unordered_flat_map/Map.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "Hash.h"
-#include <boost__unordered_flat_map/include/boost/unordered_flat_map.hpp>
+#include <boost__unordered_flat_map/include/boost/unordered/unordered_flat_map.hpp>
 //#include <boost/version.hpp>
 
 static const char* MapName = "boost::unordered_flat_map";
 
 template <class Key, class Val>
-using Map = boost::unordered::unordered_flat_map<Key, Val, Hash<Key>>;
+using Map = boost::unordered_flat_map<Key, Val, Hash<Key>>;
